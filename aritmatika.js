7ed9cd296607fc88ec82c3559abb3e560a05beed
Kaaayyyynaaa <140371326+Kaaayyyynaaa@users.noreply.github.com>
2023-07-25 15:00:18 +0700

    aritmatika.js

---
+++ aritmatika.js
@@ -0,0 +1,15 @@
+let bil1=8;
+    bil2=7;
+   
+let pertambahan=bil1+bil2;    
+let pengurangan=bil1-bil2;
+let perkalian=bil1*bil2;
+let pembagian=bil1/bil2;
+let modulus=bil1%bil2;
+
+document.write("Hasil pertambahan " + pertambahan + "<br>");
+document.write("Hasil pengurangan "+pengurangan+"<br>");
+document.write("Hasil perkalian "+perkalian+"<br>");
+document.write("Hasil pembagian "+pembagian+"<br>");
+document.write("Hasil modulus "+modulus+"<br>");
+
